4e3ad7c8ea531f365709153e1874e48e4593d7ac
YouMe6110 <ume0116@naver.com>
2022-05-17 22:06:18 +0900

    DAY 02

---
+++ app.js
@@ -0,0 +1,49 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('rasize', this.resize.bind(this), false);
+        this.resize();
+
+        document.addEventListener('pointerdown', this.onDown.bind(this), false);
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+        document.addEventListener('pointerup', this.onUp.bind(this), false);
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    }
+
+    onDown(e) {
+
+    }
+
+    onMove(e) {
+
+    }
+
+    onUp(e) {
+        
+    }
+}
+
+window.onload = () => {
+    new App();
+}
